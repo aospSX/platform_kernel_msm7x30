e9840fe83e3dd21b112cd96cbb7e96b288ccf280
Brian Swetland <swetland@google.com>
2009-07-01 17:58:37 -0700

    msm: cleanup smd, separate debugfs support
    - pull debug code into smd_debug.c
    - move necessary structures and defines into smd_private.h
    - fix some comment formatting, etc
    
    Signed-off-by: Brian Swetland <swetland@google.com>
    Signed-off-by: Daniel Walker <dwalker@codeaurora.org>

--- arch/arm/mach-msm/smd_private.h
+++ arch/arm/mach-msm/smd_private.h
@@ -21,6 +21,10 @@
 #ifndef _ARCH_ARM_MACH_MSM_MSM_SMD_PRIVATE_H_
 #define _ARCH_ARM_MACH_MSM_MSM_SMD_PRIVATE_H_
 
+#include <linux/platform_device.h>
+#include <linux/spinlock.h>
+#include <linux/list.h>
+
 struct smem_heap_info {
 	unsigned initialized;
 	unsigned free_offset;
@@ -278,6 +282,85 @@ typedef enum {
 #define SMD_KIND_UNKNOWN	0x000
 #define SMD_KIND_STREAM		0x100
 #define SMD_KIND_PACKET		0x200
+
+struct smd_alloc_elm {
+	char name[20];
+	uint32_t cid;
+	uint32_t ctype;
+	uint32_t ref_count;
+};
+
+struct smd_half_channel {
+	unsigned state;
+	unsigned char fDSR;
+	unsigned char fCTS;
+	unsigned char fCD;
+	unsigned char fRI;
+	unsigned char fHEAD;
+	unsigned char fTAIL;
+	unsigned char fSTATE;
+	unsigned char fUNUSED;
+	unsigned tail;
+	unsigned head;
+} __attribute__((packed));
+
+struct smd_shared_v1 {
+	struct smd_half_channel ch0;
+	unsigned char data0[SMD_BUF_SIZE];
+	struct smd_half_channel ch1;
+	unsigned char data1[SMD_BUF_SIZE];
+};
+
+struct smd_shared_v2 {
+	struct smd_half_channel ch0;
+	struct smd_half_channel ch1;
+};	
+
+struct smd_channel {
+	volatile struct smd_half_channel *send;
+	volatile struct smd_half_channel *recv;
+	unsigned char *send_data;
+	unsigned char *recv_data;
+
+	unsigned fifo_mask;
+	unsigned fifo_size;
+	unsigned current_packet;
+	unsigned n;
+
+	struct list_head ch_list;
+
+	void *priv;
+	void (*notify)(void *priv, unsigned flags);
+
+	int (*read)(struct smd_channel *ch, void *data, int len);
+	int (*write)(struct smd_channel *ch, const void *data, int len);
+	int (*read_avail)(struct smd_channel *ch);
+	int (*write_avail)(struct smd_channel *ch);
+
+	void (*update_state)(struct smd_channel *ch);
+	unsigned last_state;
+	void (*notify_other_cpu)(void);
+	unsigned type;
+
+	char name[32];
+	struct platform_device pdev;
+};
+
+#define SMD_TYPE_MASK		0x0FF
+#define SMD_TYPE_APPS_MODEM	0x000
+#define SMD_TYPE_APPS_DSP	0x001
+#define SMD_TYPE_MODEM_DSP	0x002
+
+#define SMD_KIND_MASK		0xF00
+#define SMD_KIND_UNKNOWN	0x000
+#define SMD_KIND_STREAM		0x100
+#define SMD_KIND_PACKET		0x200
+
+extern struct list_head smd_ch_closed_list;
+extern struct list_head smd_ch_list;
+
+extern spinlock_t smd_lock;
+extern spinlock_t smem_lock;
 
 void *smem_find(unsigned id, unsigned size);
 void *smem_item(unsigned id, unsigned *size);
