613056527d6845ceaf17795c7ebb8f3604689a57
Jarod Wilson <jarod@redhat.com>
2010-10-16 21:36:43 -0300

    lirc_dev: get irctl from irctls by inode again
    Can't explain it (yet), but I've seen the 'get irctl via private_data'
    setup fail for a number of people (ioctl called before its filled in?),
    so lets go back to a variant of the old way, but one that still works
    with unlocked_ioctl.
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/IR/lirc_dev.c
+++ drivers/media/IR/lirc_dev.c
@@ -433,7 +433,6 @@ int lirc_dev_fop_open(struct inode *inode, struct file *file)
 		retval = -ENODEV;
 		goto error;
 	}
-	file->private_data = ir;
 
 	dev_dbg(ir->d.dev, LOGHEAD "open called\n", ir->d.name, ir->d.minor);
 
@@ -531,7 +530,7 @@ long lirc_dev_fop_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
 	__u32 mode;
 	int result = 0;
-	struct irctl *ir = file->private_data;
+	struct irctl *ir = irctls[iminor(file->f_dentry->d_inode)];
 
 	if (!ir) {
 		printk(KERN_ERR "lirc_dev: %s: no irctl found!\n", __func__);
