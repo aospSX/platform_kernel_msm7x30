8151f7aa6a951ee689539845853d96e2f76e6c09
steppnasty <steppnasty@gmail.com>
2015-04-19 09:49:17 -0400

    Build fix for kernel patches Fix for the following patches: workqueue: kill RT workqueue bridge: add per bridge device controls for invoking iptables net: fix 64 bit counters on 32 bit arches

--- net/netfilter/xt_qtaguid.c
+++ net/netfilter/xt_qtaguid.c
@@ -790,7 +790,7 @@ static int iface_stat_all_proc_read(char *page, char **num_items_returned,
 	int item_index = 0;
 	int len;
 	struct iface_stat *iface_entry;
-	const struct net_device_stats *stats;
+	const struct rtnl_link_stats64 dev_stats, *stats;
 	struct rtnl_link_stats64 no_dev_stats = {0};
 
 	if (unlikely(module_passive)) {
@@ -816,7 +816,8 @@ static int iface_stat_all_proc_read(char *page, char **num_items_returned,
 			continue;
 
 		if (iface_entry->active) {
-			stats = dev_get_stats(iface_entry->net_dev);
+			stats = dev_get_stats(iface_entry->net_dev,
+					      &dev_stats);
 		} else {
 			stats = &no_dev_stats;
 		}
@@ -952,10 +953,10 @@ static struct iface_stat *iface_alloc(struct net_device *net_dev)
 static void iface_check_stats_reset_and_adjust(struct net_device *net_dev,
 					       struct iface_stat *iface)
 {
-	const struct net_device_stats *stats;
+	const struct net_device_stats dev_stats, *stats;
 	bool stats_rewound;
 
-	stats = dev_get_stats(net_dev);
+	stats = dev_get_stats(net_dev, &dev_stats);
 	/* No empty packets */
 	stats_rewound =
 		(stats->rx_bytes < iface->last_known[IFS_RX].bytes)
@@ -1173,10 +1174,10 @@ data_counters_update(struct data_counters *dc, int set,
  */
 static void iface_stat_update(struct net_device *net_dev, bool stash_only)
 {
-	const struct net_device_stats *stats;
+	const struct rtnl_link_stats64 dev_stats, *stats;
 	struct iface_stat *entry;
 
-	stats = dev_get_stats(net_dev);
+	stats = dev_get_stats(net_dev, &dev_stats);
 	spin_lock_bh(&iface_stat_list_lock);
 	entry = get_iface_entry(net_dev->name);
 	if (entry == NULL) {
